3d244acdfe7c47feeb3d8a50bbeb2a2b452dc9a6
Jacques Malgrange <jacques.malgrange@gmail.com>
2017-12-30 12:14:01 +0100

    1.5mod

---
+++ uno/w3cssDefault.php
@@ -0,0 +1,58 @@
+<?php
+// https://www.w3schools.com/w3css/
+$Uw3 = array(
+'card'=>array(
+	'w3-card'=>'w3-card-2 w3-dark-grey',
+	'w3-button'=>'w3-button w3-green'
+),
+'input'=>array(
+	'w3-container'=>'w3-container',
+	'w3-input'=>'w3-input',
+	'w3-check'=>'w3-check',
+	'w3-radio'=>'w3-radio',
+	'w3-select'=>'w3-select',
+	'w3-button'=>'w3-button'
+),
+'dropdown'=>array(
+	'w3-dropdown-hover'=>'w3-dropdown-hover',
+	'w3-dropdown-content'=>'w3-dropdown-content',
+	'w3-dropdown-click'=>'w3-dropdown-click',
+	'w3-button'=>'w3-button',
+	'w3-badge'=>'w3-badge w3-green'
+),
+'modal'=>array(
+	'w3-modal'=>'w3-modal',
+	'w3-modal-content'=>'w3-modal-content w3-card-4',
+	'header'=>'w3-container w3-padding-large w3-teal',
+	'xclose'=>'w3-button w3-display-topright',
+	'content'=>'w3-container w3-padding',
+	'footer'=>'w3-container w3-padding-small w3-teal'
+),
+'grid'=>array(
+	'w3-row'=>'w3-row',
+	'w3-row-padding'=>'w3-row-padding',
+	'w3-col'=>'w3-col',
+	'w3-rest'=>'w3-rest'
+),
+'Uno'=>array(
+	'div-pagesContent'=>'w3-container',
+	'div-blocChap'=>'w3-section',
+	'h2-nav1'=>'',
+	'h2-nav2'=>''
+),
+'UnoMenu'=>array(
+	'large-bar'=>'w3-bar w3-black',
+	'small-bar'=>'w3-bar-block w3-black',
+	'large-bar-item'=>'w3-bar-item w3-button',
+	'large-bar-parent-item'=>'w3-bar-item w3-button',
+	'large-bar-sub-item'=>'w3-bar-item w3-button',
+	'small-bar-open'=>'w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium',
+	'small-bar-item'=>'w3-bar-item w3-button',
+	'w3-dropdown'=>'w3-dropdown-hover',
+	'w3-dropdown-content'=>'w3-dropdown-content w3-black w3-card-2',
+	'large-hide'=>'w3-hide-small',
+	'small-hide'=>'w3-hide w3-hide-large w3-hide-medium',
+	'activeMenuClass'=>'w3-dark-grey'
+)
+);
+?>
